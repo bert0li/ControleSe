a511739b06d0074bcd15a88cbbd0117f6e2d1bdd
bertoli <bert0li@outlook.com.br>
2021-09-17 22:40:05 -0300

    ;

--- ControleSe/Operacoes/DividasForm.cs
+++ ControleSe/Operacoes/DividasForm.cs
@@ -35,6 +35,9 @@ namespace ControleSe.Operacoes
             grid.AutoGenerateColumns = false;
         }
 
+        /// <summary>
+        ///  TESTE
+        /// </summary>
         private void LinhaSeleciona(/*DataGridViewCellEventArgs e*/)
         {
             grid.RowEnter += (s, e) =>
