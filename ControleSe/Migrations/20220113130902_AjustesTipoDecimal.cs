aaacb7dc4b9c70d715677d4049e3e30763c563dd
bertoli <bertoli@webeasy.com.br>
2022-01-13 10:16:13 -0300

    Alteração no tipo de dado de valores do tipo decimal com dois números após a virgula.

---
+++ ControleSe/Migrations/20220113130902_AjustesTipoDecimal.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ControleSe.Migrations
+{
+    public partial class AjustesTipoDecimal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ValorEntrada",
+                table: "Entrada",
+                type: "decimal(65,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Valor",
+                table: "Divida",
+                type: "decimal(65,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)",
+                oldDefaultValue: 0m);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalCobre",
+                table: "Cofre",
+                type: "decimal(65,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ValorEntrada",
+                table: "Entrada",
+                type: "decimal(65,30)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Valor",
+                table: "Divida",
+                type: "decimal(65,30)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,2)",
+                oldDefaultValue: 0m);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalCobre",
+                table: "Cofre",
+                type: "decimal(65,30)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,2)");
+        }
+    }
+}
